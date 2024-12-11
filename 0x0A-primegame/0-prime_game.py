754a8c5f14b830984005da7d3fcc3e813db5f54b
Collinsgra <103281879+Collinsgra@users.noreply.github.com>
2024-12-12 01:33:48 +0300

    Updated 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-""" Prime Game algo """
+""" Prime Game algorythm """
 
 
 def is_prime(n):
-    """ Checks if a number given n is a prime number """
+    """ Checks if a number given n is a prime-number """
     for i in range(2, int(n ** 0.5) + 1):
         if not n % i:
             return False
